a43570c7e9b2b33a255c2e4922655a26931a24ba
ben <bencoral26@gmail.com>
2023-08-31 13:52:08 +0300

    basic

--- app.py
+++ app.py
@@ -3,5 +3,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/home.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup.html')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
